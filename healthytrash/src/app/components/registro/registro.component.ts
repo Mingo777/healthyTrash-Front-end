9b16c053e7de7cf34a6bbf90b6c7e818130c4150
Mingorance777 <cmingoranceespigares@gmail.com>
2021-12-13 15:46:02 +0100

    Creacion de registro

--- healthytrash/src/app/components/registro/registro.component.ts
+++ healthytrash/src/app/components/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  formulario: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.formulario = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      description: new FormControl('', [Validators.required]),
+      Author: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+      date: new FormControl(new Date().toLocaleDateString('en-CA'), [Validators.required]),
+      category: new FormControl('', [Validators.required])
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() { }
+
+
+  checkError(controlName: string, error: string, touched: boolean): boolean | undefined {
+    return touched ? this.formulario.get(controlName)?.hasError(error) && this.formulario.get(controlName)?.touched : this.formulario.get(controlName)?.hasError(error);
+  }
 }
