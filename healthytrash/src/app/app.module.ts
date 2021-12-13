d1ec793dcc4969e8b94886ad80310b6526eb996f
Mingorance777 <cmingoranceespigares@gmail.com>
2021-12-13 14:11:52 +0100

    Creacion de componentes.

--- healthytrash/src/app/app.module.ts
+++ healthytrash/src/app/app.module.ts
@@ -1,29 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from '../app/components/formulario/formulario.component';
 import { NavBarComponent } from '../app/components/nav-bar/nav-bar.component';
 import { HomeComponent } from '../app/components/home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { RecetasComponent } from './components/recetas/recetas.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent,
     NavBarComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    BlogComponent,
+    ContactoComponent,
+    RecetasComponent,
+    RegistroComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
 
   ],
   providers: [],
