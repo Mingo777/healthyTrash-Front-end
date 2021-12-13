43dc2b4fe62c7296bcfe783db9676dc0981b377b
Mingorance777 <cmingoranceespigares@gmail.com>
2021-12-13 14:17:11 +0100

    Creacion de rutas

--- healthytrash/src/app/app-routing.module.ts
+++ healthytrash/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RecetasComponent } from './components/recetas/recetas.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'home', component: HeroComponent },
+  { path: 'recetas', component: RecetasComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'contacto', component: ContactoComponent }
 ];
 
 @NgModule({
